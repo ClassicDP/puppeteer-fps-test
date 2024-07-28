2855f8202657663ee6b1b5afe07ca8fc58280f96
dp <pasynkov.d@gmail.com>
2024-07-28 21:50:29 +0300

    Loading course indexes, scroll + player

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-import { webkit } from 'playwright';
+import {webkit} from 'playwright';
 import fs from 'fs';
 import http from 'http';
 import cheerio from 'cheerio';
 import fetch from 'node-fetch';
-import WebSocket, { WebSocketServer } from 'ws';
+import WebSocket, {WebSocketServer} from 'ws';
+import * as os from "node:os";
 
 const getStockData = async () => {
     try {
@@ -19,7 +20,7 @@ const getStockData = async () => {
         const usdRate = getValue("USD");
         const cnyRate = getValue("CNY");
 
-        return { usdRate, cnyRate };
+        return {usdRate, cnyRate};
     } catch (error) {
         console.error('Error fetching stock data:', error);
         return null;
@@ -50,44 +51,74 @@ const proxyServer = http.createServer(async (req, res) => {
 
 // Create WebSocket server
 const server = http.createServer();
-const wss = new WebSocketServer({ server });
+const wss = new WebSocketServer({server});
 let clients = [];
+
+let screenshotTimes = [];
+let sendTimes = [];
 
 wss.on('connection', (ws) => {
     clients.push(ws);
+    ws.on('message', async (message) => {
+        if (message.toString() === 'captureScreenshot') {
+            const startScreenshot = Date.now();
+            const elementHandle = await page.$('#container');
+            const boundingBox = await elementHandle.boundingBox();
+
+            const screenshotBuffer = await page.screenshot({
+                encoding: 'base64',
+                clip: boundingBox
+            });
+            const endScreenshot = Date.now();
+            screenshotTimes.push(endScreenshot - startScreenshot);
+
+            const imageBuffer = Buffer.from(screenshotBuffer, 'base64');
+            const startSend = Date.now();
+            clients.forEach((client) => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(imageBuffer);
+                }
+            });
+            const endSend = Date.now();
+            sendTimes.push(endSend - startSend);
+        }
+    });
     ws.on('close', () => {
         clients = clients.filter((client) => client !== ws);
     });
 });
 
+function logAverageTimes() {
+    if (screenshotTimes.length > 0 && sendTimes.length > 0) {
+        const averageScreenshotTime = screenshotTimes.reduce((a, b) => a + b, 0) / screenshotTimes.length;
+        const averageSendTime = sendTimes.reduce((a, b) => a + b, 0) / sendTimes.length;
+        console.log(`Average Screenshot Time: ${averageScreenshotTime.toFixed(2)}ms`);
+        console.log(`Screenshot Count per sec: ${screenshotTimes.length}`);
+        console.log(`Average Send Time: ${averageSendTime.toFixed(2)}ms`);
+        screenshotTimes = [];
+        sendTimes = [];
+        // Получение общей памяти системы в байтах
+        const totalMemory = os.totalmem();
+
+// Получение свободной памяти системы в байтах
+        const freeMemory = os.freemem();
+
+// Вывод информации в консоль
+        console.log(`Общая память: ${totalMemory} байт`);
+        console.log(`Свободная память: ${freeMemory} байт`);
+    }
+}
+
+setInterval(logAverageTimes, 1000);
+
 // Launch Playwright and capture screenshots
+let page;
 (async () => {
     const browser = await webkit.launch();
-    const page = await browser.newPage();
+    page = await browser.newPage();
     const htmlContent = fs.readFileSync('index.html', 'utf8');
     await page.setContent(htmlContent);
-    await page.setViewportSize({ width: 96, height: 32 });
-
-    async function captureScreenshot() {
-        const elementHandle = await page.$('#container');
-        const boundingBox = await elementHandle.boundingBox();
-        const screenshotBuffer = await page.screenshot({
-            encoding: 'base64',
-            clip: boundingBox
-        });
-
-        const imageBuffer = Buffer.from(screenshotBuffer, 'base64');
-
-        clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(imageBuffer);
-            }
-        });
-
-        setTimeout(captureScreenshot, 33); // Capture at ~30fps
-    }
-
-    captureScreenshot();
+    await page.setViewportSize({width: 96, height: 32});
 
     process.on('exit', async () => {
         await browser.close();
